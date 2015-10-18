47c23ff10ebec658ab3b167d11ebab24c2975d57
Grzegorz Laganowski <g.laganowski94@gmail.com>
2015-10-18 07:25:49 +0200

    pytKom01: Development branch Refactored the code and added all made regex patterns

--- template.py
+++ template.py
@@ -1,82 +1,67 @@
 # -*- coding: utf-8 -*-
 
-__author1__ = 'Kamil'
-__author2__ = 'Grzegorz'
 
 import os
 import sys
 import re
 import codecs
 
+def list_unique_counter(content, pattern):
+    r = re.compile(pattern)
+    list = r.findall(content)
+    counts = 0
+    if list:
+        unique_words = set(list)
+        counts = len(unique_words)
+
+    return counts
+
+
+
 def processFile(filepath):
     fp = codecs.open(filepath, 'rU', 'iso-8859-2')
-
     content = fp.read()
 
-    def liczbaRoznychElementowListy(lista):
-        liczba=0
-        ilosc=0
-        for i in range(len(lista)):
-            if lista:
-                element = lista.pop()
-                liczba=liczba+1
-                ilosc=lista.count(element)
-                for j in range(ilosc):
-                    lista.remove(element)
-        return liczba
+    ### section P ###
+    pattern_section_p = r'<P>([\W|\w]*)</P>'
+    r = re.compile(pattern_section_p)
+    section_P = r.findall(content)
+    #################
 
+    #AUTOR
+    autor_pattern = re.search(r'<META NAME="AUTOR" CONTENT="(\D*)">',content)
+    autor = autor_pattern.group(1)
 
-    ######### email ####################
-    pattern = r'([\w-]+)(\.\w+)*@(\w+)(-\w+)*\.[a-zA-Z0-9]+(-\w+)*(\.[a-zA-Z]+(-\w+)*)*'
+    #SECTION DESCRIPTION
+    section_description_pattern = re.search(r'<META NAME="DZIAL" CONTENT="(.*)"',content)
+    section_description = section_description_pattern.group(1)
 
-#     .@q.q.q.q.pl
-# q@@qp._
-# sq_12@p-.pl
-# qwerty.qwerty25@gmail._
-# qwerty.qwerty.12@co-m.f-888e.co-m.p-l.p-l
-# qwerty@student.agh.edu.pl
-# qwerty@student.agh.edu..pl
-# qwerty..qwerty.12@fke.com
-# qwerty.qwerty.12@.fke.com
-# .qwerty.qwerty.12@fke.com
-# qwerty.qwerty.12@fke.com.
-# qwerty.qwerty.12.@fke.com
+    #KEY WORDS
+    key_words = re.findall(r'<META NAME="KLUCZOWE_\d+" CONTENT="(.*)">', content)
 
-    r = re.compile(pattern)
-    m = r.findall(content)
-    emaile = liczbaRoznychElementowListy(m)
-    ######################################
+    #EMAILS
+    email_pattern = r'([\w-]+)(\.\w+)*@(\w+)(-\w+)*\.[a-zA-Z0-9]+(-\w+)*(\.[a-zA-Z]+(-\w+)*)*'
+    email_counts = list_unique_counter(content, email_pattern)
 
+    #SHORTCUTS
+    shortcut_pattern = r'(\s[a-zA-Z]{1,3}\.)'
+    shortcut_counts = list_unique_counter(' '.join(section_P), shortcut_pattern)
 
-    pattern2 = r'<META NAME="AUTOR" CONTENT="(\D*)">'   #moj kod
-    r = re.compile(pattern2)
-    autor = r.findall(content)
+    #FLOATS
+    float_pattern = r' (?:-?(?:(?:\d+\.\d?)|(?:\d*\.\d+))(?:[eE][+-]\d+)?)'
+    float_counts = list_unique_counter(' '.join(section_P), float_pattern)
 
-    pattern3 = r'<META NAME="DZIAL" CONTENT="(.*)">'   #moj kod
-    r = re.compile(pattern3)
-    dzial = r.findall(content)
+    #INTEGERS
+    integer_pattern = r'\s(([-]{,1}([1-9]\d{1,3}|[1-2]\d{4}|3[0-1]\d{3}|32[0-6]\d{2}|327[0-5]\d|3276[0-7]))|-32768|\d)\s'
+    integer_counts = list_unique_counter(' '.join(section_P), integer_pattern)
 
-    pattern3 = r'<META NAME="KLUCZOWE_\d*" CONTENT="(\D*)">'   #moj kod (slowo kluczowe jako cyfra!)
-    r = re.compile(pattern3)
-    kluczowe = r.findall(content)
+    #SENTENCES
+    sentences_pattern = r'([\dA-ZĄĆĘŃÓŚŁŻŹ]([A-ZĄĆĘŃÓŚŁŻŹa-ząćęńóśżłź\s,\|\)\/\-\+\=\(\*&^%\$#@!\d]*|(\s[a-zA-Z]{1,3}\.\s))+)'
+    r = re.compile(sentences_pattern)
+    sentences_list = r.findall(' '.join(section_P))
+    sentences_counts = len(sentences_list)
 
-
-    ########## sekcja P ###############
-    pattern4 = r'<P>([\W|\w]*)</P>'   #moj kod
-    r = re.compile(pattern4)
-    sekcjaP = r.findall(content)
-    #########################################
-
-
-
-    ########### skroty ##################
-    pattern5 = r'(\s[a-zA-Z]{1,3}\.)'
-    r = re.compile(pattern5)
-    skr = r.findall(' '.join(sekcjaP))
-    skrotyLiczba = liczbaRoznychElementowListy(skr)
-    #####################################
-
-    ########### daty ####################### dorobic ze daty sa poprawne
+    #DATES
     def poprawnaDataDMR(listaDMR):
         lista=[]
         for i in listaDMR:
@@ -121,71 +106,30 @@ def processFile(filepath):
 
     pattern6 = r'((31[-./](01|03|05|07|08|10|12)[-./]\d\d\d\d)|(((0[1-9])|(1[0-9])|2[0-9])[-./]((0[1-9])|(1[0-2]))[-./]\d\d\d\d)|(30[./](01|03|04|05|06|07|08|09|10|11|12)[-./]\d\d\d\d))'
     r = re.compile(pattern6)
-    data = r.findall(' '.join(sekcjaP))
-   # print(data.group(1))
-
-
+    data = r.findall(' '.join(section_P))
 
     pattern7 = r'(\d\d\d\d[-./]31[-./](01|03|05|07|08|10|12)|(\d\d\d\d[-./]((0[1-9])|(1[0-9])|2[0-9])[-./]((0[1-9])|(1[0-2])))|(\d\d\d\d[./]30[-./](01|03|04|05|06|07|08|09|10|11|12)))'
     r = re.compile(pattern7)
-    data2 = r.findall(' '.join(sekcjaP))
+    data2 = r.findall(' '.join(section_P))
     ###########################################
-
-
-
-    #r'(-?((\d+\.?\d?)|(\d*\.?\d+))(e[+-]\d+)?)'
-
-    ########## liczby zmiennoprzecinkowe ##############
-    pattern7 = r' (?:-?(?:(?:\d+\.\d?)|(?:\d*\.\d+))(?:[eE][+-]\d+)?)'
-    r = re.compile(pattern7)
-    zmiennoprzecinkowe = r.findall(' '.join(sekcjaP))
-
-    liczbyzm = liczbaRoznychElementowListy(zmiennoprzecinkowe)
-    ####################################################
-
-    ########## liczby calkowite ##############
-    pattern7 = r'\s(([-]{,1}([1-9]\d{1,3}|[1-2]\d{4}|3[0-1]\d{3}|32[0-6]\d{2}|327[0-5]\d|3276[0-7]))|-32768|\d)\s'
-    r = re.compile(pattern7)
-    calk = r.findall(' '.join(sekcjaP))
-
-    liczbyca = liczbaRoznychElementowListy(calk)
-    ####################################################
-
-    ########## zdania ##############
-    pattern8 =r'([\dA-ZĄĆĘŃÓŚŁŻŹ]([A-ZĄĆĘŃÓŚŁŻŹa-ząćęńóśżłź\s,\|\)\/\-\+\=\(\*&^%\$#@!\d]*|(\s[a-zA-Z]{1,3}\.\s))+)'
-    #r'([\dA-ZĄĆĘŃÓŚŁŻŹ](?:[A-ZĄĆĘŃÓŚŁŻŹa-ząćęńóśżłź\s,\|\)\/\-\+\=\(\*\&\^\%\$\#\@\!\d]*(?:\s[a-zA-Z]{1,3}\.\s)?)[\.!?\n]+\s)'
-     # r'[\dA-ZĄĆĘŃÓŚŁŻŹ]([A-ZĄĆĘŃÓŚŁŻŹa-ząćęńóśżłź\s,\|\)\/\-\+\=\(\*\&\^\%\$\#\@\!\d])*[\.!?\n]+\s'
-        #r'[A-ZĄĆĘŃÓŚŁŻŹ][a-ząćęńóśżłź\s,\|\)\/\-\+\=\(\*\&\^\%\$\#\@\!\d]*'
-    r = re.compile(pattern8)
-    zda = r.findall(' '.join(sekcjaP))
-   # zdania = liczbaRoznychElementowListy(zda)
-    print("aaa")
-    print(zda)
-    print("aaa")
-    ####################################################
-
-
-
-
 
 
     fp.close()
     print("nazwa pliku:", filepath)
-    print("autor:",autor[0])
-    print("dzial:",dzial[0])
-    print("slowa kluczowe:",', '.join(kluczowe))
-    print("liczba zdan:")
-    print("liczba skrotow:",skrotyLiczba)
-    print("liczba liczb calkowitych z zakresu int:",liczbyca)
-    print("liczba liczb zmiennoprzecinkowych:",liczbyzm)
-    print("liczba dat:",liczbaDat(data,data2))
-    print("liczba adresow email:",emaile)
+    print("autor: ", autor)
+    print("dzial: ", section_description)
+    print("slowa kluczowe:", key_words)
+    print("liczba zdan: ", sentences_counts)
+    print("liczba skrotow: ", shortcut_counts)
+    print("liczba liczb calkowitych z zakresu int: ", integer_counts)
+    print("liczba liczb zmiennoprzecinkowych: ", float_counts)
+    print("liczba dat: ", liczbaDat(data,data2))
+    print("liczba adresow email:", email_counts)
     print("\n")
 
 
-
 try:
-    path = sys.argv[1]
+    path = os.path.join(os.getcwd(), "1993-01") #sys.argv[1]
 except IndexError:
     print("Brak podanej nazwy katalogu")
     sys.exit(0)
